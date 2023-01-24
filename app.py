a0ee8d698178c6605295215670536096938ef137
lite <blackbo2@naver.com>
2023-01-24 22:02:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import streamlit as st
+import altair as alt
 from pandas_datareader import data as pdr
 from datetime import datetime, timedelta
 import yfinance as yf
 from streamlit_extras.let_it_rain import rain
+import pandas as pd
+import numpy as np
 
 # config
 st.set_page_config(layout="wide", page_title="Hello", page_icon="🏠")
 
 streamlit_style = """
 			<style>
-			@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
+@font-face {
+    font-family: 'ChosunGu';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/ChosunGu.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 
 			html, body, [class*="css"]  {
-			font-family: 'Nanum Gothic', sans-serif;
+			font-family: 'ChosunGu', sans-serif;
 			}
 			</style>
 			"""
@@ -20,9 +28,9 @@ st.markdown(streamlit_style, unsafe_allow_html=True)
 
 # rain
 rain(
-    emoji="❄️",
-    font_size=13,
-    falling_speed=8,
+    emoji="🌟",
+    font_size=10,
+    falling_speed=22,
     animation_length="infinite",
 )
 
@@ -58,6 +66,31 @@ st.markdown("----", unsafe_allow_html=True)
 
 # https://finance.yahoo.com/quote/005930.KS?p=005930.KS
 dr = pdr.get_data_yahoo(dict[option], start_date, end_date)
+
+dr.reset_index(inplace=True)
+
+dr['Date'] = pd.to_datetime(dr['Date']).dt.date
+st.dataframe( dr)
+
+chart_data = alt.Chart(dr).mark_line(    
+    point={
+      "filled": True,
+      "fill": "red"
+    }).encode(
+    alt.X('Date', title='날짜', axis=alt.Axis(tickCount="day")),
+    alt.Y('Close', title='가격'),
+    tooltip='Close',
+)
+
+nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['x'], empty='none')
+
+st.write('''마감 가격''')
+st.altair_chart(chart_data, use_container_width=True)
+
+
+
+
 
 st.write('''마감 가격''')
 st.line_chart(dr.Close)
